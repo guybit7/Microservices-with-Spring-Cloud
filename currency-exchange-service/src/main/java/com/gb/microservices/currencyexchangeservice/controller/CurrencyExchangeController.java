10b4dae7fe14247df06a6ddc1de3051d38fca5f9
XGQ376 <guybiton7p@gmail.com>
2022-06-22 15:23:55 +0300

    Setting up Dynamic Port in the the Response

--- currency-exchange-service/src/main/java/com/gb/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/gb/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.gb.microservices.currencyexchangeservice.controller;
 
 import com.gb.microservices.currencyexchangeservice.entity.ExchangeValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,12 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    @Autowired
+    private Environment environment;
     @GetMapping("/current-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        return new ExchangeValue(100L, from, to, BigDecimal.valueOf(65));
+        ExchangeValue exchangeValue = new ExchangeValue(100L, from, to, BigDecimal.valueOf(65));
+        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        return exchangeValue;
     }
 }
--- currency-exchange-service/src/main/java/com/gb/microservices/currencyexchangeservice/entity/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/gb/microservices/currencyexchangeservice/entity/ExchangeValue.java
@@ -8,6 +8,8 @@ public class ExchangeValue {
     private String to;
     private BigDecimal conversionMultiple;
 
+    private int port;
+
     public ExchangeValue() {
 
     }
@@ -49,5 +51,13 @@ public class ExchangeValue {
 
     public void setConversionMultiple(BigDecimal conversionMultiple) {
         this.conversionMultiple = conversionMultiple;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
 }
